b576f0275615eee6f1556dbbae15dfe562e5a5cc
xela <thegamerale97@gmail.com>
2022-05-20 15:29:23 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_perimetro
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grp_coordinate = New System.Windows.Forms.GroupBox()
+        Me.btn_fine = New System.Windows.Forms.Button()
+        Me.btn_calcola = New System.Windows.Forms.Button()
+        Me.lbl_risultato = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txt_y3 = New System.Windows.Forms.TextBox()
+        Me.txt_y2 = New System.Windows.Forms.TextBox()
+        Me.txt_y1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txt_x3 = New System.Windows.Forms.TextBox()
+        Me.txt_x2 = New System.Windows.Forms.TextBox()
+        Me.txt_x1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.grp_coordinate.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grp_coordinate
+        '
+        Me.grp_coordinate.Controls.Add(Me.btn_fine)
+        Me.grp_coordinate.Controls.Add(Me.btn_calcola)
+        Me.grp_coordinate.Controls.Add(Me.lbl_risultato)
+        Me.grp_coordinate.Controls.Add(Me.Label10)
+        Me.grp_coordinate.Controls.Add(Me.txt_y3)
+        Me.grp_coordinate.Controls.Add(Me.txt_y2)
+        Me.grp_coordinate.Controls.Add(Me.txt_y1)
+        Me.grp_coordinate.Controls.Add(Me.Label7)
+        Me.grp_coordinate.Controls.Add(Me.Label8)
+        Me.grp_coordinate.Controls.Add(Me.Label9)
+        Me.grp_coordinate.Controls.Add(Me.txt_x3)
+        Me.grp_coordinate.Controls.Add(Me.txt_x2)
+        Me.grp_coordinate.Controls.Add(Me.txt_x1)
+        Me.grp_coordinate.Controls.Add(Me.Label6)
+        Me.grp_coordinate.Controls.Add(Me.Label5)
+        Me.grp_coordinate.Controls.Add(Me.Label4)
+        Me.grp_coordinate.Controls.Add(Me.Label3)
+        Me.grp_coordinate.Controls.Add(Me.Label2)
+        Me.grp_coordinate.Controls.Add(Me.Label1)
+        Me.grp_coordinate.Location = New System.Drawing.Point(12, 12)
+        Me.grp_coordinate.Name = "grp_coordinate"
+        Me.grp_coordinate.Size = New System.Drawing.Size(429, 188)
+        Me.grp_coordinate.TabIndex = 0
+        Me.grp_coordinate.TabStop = False
+        Me.grp_coordinate.Text = "Coordinate"
+        '
+        'btn_fine
+        '
+        Me.btn_fine.Location = New System.Drawing.Point(354, 159)
+        Me.btn_fine.Name = "btn_fine"
+        Me.btn_fine.Size = New System.Drawing.Size(75, 23)
+        Me.btn_fine.TabIndex = 18
+        Me.btn_fine.Text = "Fine"
+        Me.btn_fine.UseVisualStyleBackColor = True
+        '
+        'btn_calcola
+        '
+        Me.btn_calcola.Location = New System.Drawing.Point(6, 159)
+        Me.btn_calcola.Name = "btn_calcola"
+        Me.btn_calcola.Size = New System.Drawing.Size(75, 23)
+        Me.btn_calcola.TabIndex = 17
+        Me.btn_calcola.Text = "Calcola"
+        Me.btn_calcola.UseVisualStyleBackColor = True
+        '
+        'lbl_risultato
+        '
+        Me.lbl_risultato.AutoSize = True
+        Me.lbl_risultato.Location = New System.Drawing.Point(114, 136)
+        Me.lbl_risultato.Name = "lbl_risultato"
+        Me.lbl_risultato.Size = New System.Drawing.Size(0, 15)
+        Me.lbl_risultato.TabIndex = 16
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(6, 136)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(62, 15)
+        Me.Label10.TabIndex = 15
+        Me.Label10.Text = "Perimetro:"
+        '
+        'txt_y3
+        '
+        Me.txt_y3.Location = New System.Drawing.Point(323, 104)
+        Me.txt_y3.Name = "txt_y3"
+        Me.txt_y3.Size = New System.Drawing.Size(100, 23)
+        Me.txt_y3.TabIndex = 14
+        '
+        'txt_y2
+        '
+        Me.txt_y2.Location = New System.Drawing.Point(323, 57)
+        Me.txt_y2.Name = "txt_y2"
+        Me.txt_y2.Size = New System.Drawing.Size(100, 23)
+        Me.txt_y2.TabIndex = 13
+        '
+        'txt_y1
+        '
+        Me.txt_y1.Location = New System.Drawing.Point(323, 16)
+        Me.txt_y1.Name = "txt_y1"
+        Me.txt_y1.Size = New System.Drawing.Size(100, 23)
+        Me.txt_y1.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(283, 107)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(20, 15)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Y3"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(283, 60)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(20, 15)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "Y2"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(283, 19)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(20, 15)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Y1"
+        '
+        'txt_x3
+        '
+        Me.txt_x3.Location = New System.Drawing.Point(154, 104)
+        Me.txt_x3.Name = "txt_x3"
+        Me.txt_x3.Size = New System.Drawing.Size(100, 23)
+        Me.txt_x3.TabIndex = 8
+        '
+        'txt_x2
+        '
+        Me.txt_x2.Location = New System.Drawing.Point(154, 57)
+        Me.txt_x2.Name = "txt_x2"
+        Me.txt_x2.Size = New System.Drawing.Size(100, 23)
+        Me.txt_x2.TabIndex = 7
+        '
+        'txt_x1
+        '
+        Me.txt_x1.Location = New System.Drawing.Point(154, 16)
+        Me.txt_x1.Name = "txt_x1"
+        Me.txt_x1.Size = New System.Drawing.Size(100, 23)
+        Me.txt_x1.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(114, 107)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(20, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "X3"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(114, 60)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(20, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "X2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(114, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "X1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Terzo vertice"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Secondo vertice"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Primo vertice"
+        '
+        'frm_perimetro
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 212)
+        Me.Controls.Add(Me.grp_coordinate)
+        Me.Name = "frm_perimetro"
+        Me.Text = "Perimetro triangolo"
+        Me.grp_coordinate.ResumeLayout(False)
+        Me.grp_coordinate.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents grp_coordinate As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_x3 As TextBox
+    Friend WithEvents txt_x2 As TextBox
+    Friend WithEvents txt_x1 As TextBox
+    Friend WithEvents txt_y3 As TextBox
+    Friend WithEvents txt_y2 As TextBox
+    Friend WithEvents txt_y1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lbl_risultato As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btn_calcola As Button
+    Friend WithEvents btn_fine As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,40 @@
+﻿Option Strict Off
+Option Explicit On
+
+Public Class frm_perimetro
+
+    Dim x1 As Single
+    Dim x2 As Single
+    Dim x3 As Single
+    Dim y1 As Single
+    Dim y2 As Single
+    Dim y3 As Single
+
+    Dim lato1, lato2, lato3 As Single
+
+    Dim perimetro As Single
+
+    Public Function LunghezzaLato(ByVal Xa As Single, ByVal Ya As Single, ByVal Xb As Single, ByVal Yb As Single) As Single
+        LunghezzaLato = Math.Sqrt((Xa - Xb) ^ 2 + (Ya - Yb) ^ 2)
+    End Function
+
+    Private Sub btn_calcola_Click(sender As Object, e As EventArgs) Handles btn_calcola.Click
+        x1 = Val(txt_x1.Text)
+        y1 = Val(txt_y1.Text)
+        x2 = Val(txt_x2.Text)
+        y2 = Val(txt_y2.Text)
+        x3 = Val(txt_x3.Text)
+        y3 = Val(txt_y3.Text)
+        lato1 = LunghezzaLato(x1, y1, x2, y2)
+        lato2 = LunghezzaLato(x1, y1, x3, y3)
+        lato3 = LunghezzaLato(x2, y2, x3, y3)
+
+        perimetro = lato1 + lato2 + lato3
+
+        lbl_risultato.Text = CStr(perimetro)
+    End Sub
+
+    Private Sub btn_fine_Click(sender As Object, e As EventArgs) Handles btn_fine.Click
+        End
+    End Sub
+End Class
